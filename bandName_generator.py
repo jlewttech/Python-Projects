e65180f8748d51809f504f0a2a9336a2f2a27abf
jlewttech <107934433+jlewttech@users.noreply.github.com>
2022-07-13 11:45:53 -0400

    Add files via upload

---
+++ bandName_generator.py
@@ -0,0 +1,7 @@
+greeting = "Welcome to the Band Name Generator"
+city = input("What city did you grow up in?")
+pet_type = input("What type of pet do you have?")
+
+band_name = city + " " + pet_type
+print("Your potential band name is " + band_name)
+print("Your potential band name is " + city + " " + pet_type)
